32cf38d0d21824f5d5c67cf51a4e835ae26563e2
Edouard Cunibil <admin@duael.fr>
2015-10-17 13:14:32 +0200

    Fix cart expiration to ensure that it really delete the cart

--- profiles/shoptl/modules/feature_shop_commons/js/cart-countdown.js
+++ profiles/shoptl/modules/feature_shop_commons/js/cart-countdown.js
@@ -3,26 +3,48 @@
   Drupal.behaviors.CartCountdown = {
     attach: function(context, settings) {
       $('[data-cart-countdown]').once('cart-countdown', function() {
-        var date = new Date($(this).data('cart-countdown') * 1000);
-        $(this).find('span')
-          .countdown(date, function(event) {
-            var format = '%-S seconde%!S';
-            if(event.offset.minutes > 0) {
-              format = '%-M minute%!M ' + format;
-            }
-            if(event.offset.hours > 0) {
-              format = '%-H heure%!H ' + format;
-            }
-            $(this).html(event.strftime(format));
-          })
-          .on('finish.countdown', function(event) {
-            $(this).parent()
-              .html('Votre panier a expiré !')
-              .addClass('disabled')
-              .addClass('cart-expired');
-            $(this).parents('form').find('.form-actions :input[type="submit"]')
-              .attr('disabled', true);
-          });
+        var cartId = $(this).data('cart-id');
+
+        if ($(this).data('cart-countdown') <= Date.now()) {
+          Drupal.behaviors.CartCountdown._expire(cartId);
+        }
+        else {
+          var date = new Date($(this).data('cart-countdown') * 1000);
+
+          $(this).find('span')
+            .countdown(date, function(event) {
+              var format = '%-S seconde%!S';
+              if(event.offset.minutes > 0) {
+                format = '%-M minute%!M ' + format;
+              }
+              if(event.offset.hours > 0) {
+                format = '%-H heure%!H ' + format;
+              }
+              $(this).html(event.strftime(format));
+            })
+            .on('finish.countdown', function(event) {
+              $(element).parent()
+                .html('Votre panier a expiré !')
+                .addClass('disabled')
+                .addClass('cart-expired');
+              $(element).parents('form').find('.form-actions :input[type="submit"]')
+                .attr('disabled', true);
+
+              Drupal.behaviors.CartCountdown._expire(cartId);
+            });
+        }
+      });
+    },
+
+    _expire: function(cartId) {
+      $.ajax({
+        url: 'commerce_cart_expiration/expire/' + cartId,
+        type: 'GET',
+        success: function(response) {
+          if (response.status) {
+            window.location = 'cart/expired';
+          }
+        }
       });
     }
   };
